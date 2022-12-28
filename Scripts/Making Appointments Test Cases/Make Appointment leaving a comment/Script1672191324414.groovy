7d43142d5c3a0a859e2f6d01d347a1a9a1a3325c
Ramon Assis <117400292+ramoonassis@users.noreply.github.com>
2022-12-28 17:18:52 -0300

    Add files via upload

---
+++ Scripts/Making Appointments Test Cases/Make Appointment leaving a comment/Script1672191324414.groovy
@@ -0,0 +1,57 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('https://katalon-demo-cura.herokuapp.com/')
+
+WebUI.click(findTestObject('Page_CURA Healthcare Service/Make_AppointmentBotao'))
+
+WebUI.click(findTestObject('Page_CURA Healthcare Service/Input_Usuario'))
+
+WebUI.setText(findTestObject('Page_CURA Healthcare Service/Input_Usuario'), 'John Doe')
+
+WebUI.click(findTestObject('Page_CURA Healthcare Service/Input_Senha'))
+
+WebUI.setText(findTestObject('Page_CURA Healthcare Service/Input_Senha'), 'ThisIsNotAPassword')
+
+WebUI.click(findTestObject('Page_CURA Healthcare Service/Botao_login'))
+
+WebUI.verifyTextPresent('Make Appointment', true)
+
+WebUI.click(findTestObject('Page_CURA Healthcare Service/Facility Tab'))
+
+WebUI.selectOptionByValue(findTestObject('Page_CURA Healthcare Service/Facility Tab'), 'Tokyo CURA Healthcare Center', false)
+
+WebUI.click(findTestObject('Page_CURA Healthcare Service/Apply Readmission Tag'))
+
+WebUI.click(findTestObject('Page_CURA Healthcare Service/Medicare Input 1'))
+
+WebUI.click(findTestObject('Page_CURA Healthcare Service/Comment Input'))
+
+WebUI.setText(findTestObject('Page_CURA Healthcare Service/Comment Input'), 'Agendamento para meu avô')
+
+WebUI.click(findTestObject('Page_CURA Healthcare Service/Date Input'))
+
+WebUI.setText(findTestObject('Page_CURA Healthcare Service/Date Input'), '11/06/2023')
+
+WebUI.click(findTestObject('Page_CURA Healthcare Service/Book Appointment Button'))
+
+WebUI.verifyTextPresent('Appointment Confirmation', false)
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/Testes Funcionais.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Testes Funcionais</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>24d51470-a8f9-4236-abce-219f3bc7c226</testSuiteGuid>
+   <testCaseLink>
+      <guid>8982c575-0eae-4655-b85d-b5b9906e4c38</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Test Cases (Hamburguer Menu)/Open the login page and login sucessfully (using hamburguer menu)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b8260605-e36a-47c0-b51d-5ad6cb15e477</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login Test Cases (Hamburguer Menu)/Open the login page and fail (wrong password)(using hamburguer menu)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
